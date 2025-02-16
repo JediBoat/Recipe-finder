3b1f4b0087d45e361e99255e7ec7776b181a2364
JediBoat <121259910+JediBoat@users.noreply.github.com>
2025-02-16 16:01:29 +0000

    I wrote code that setsup the database connection. Also wrote a try and cacth exception that lets the user know the connection to the db has failed when it can reach it.

--- dbapi.inc.php
+++ dbapi.inc.php
@@ -1,3 +1,15 @@
 <?php
 
-?>
+$dsn = "mysql:host=sql8.freesqldatabase.com; dbname=;";
+$dbusername = "";//Login details for the database
+$dbpass = "";
+
+try//error handling in case the database is down
+{
+    $conn = new PDO($dsn, $dbusername, $dbpass);// connection to object
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//if we get an error it throws an exception which is catched
+}
+catch(PDOException $e)
+{
+    echo "Connection failed". $e->getMessage() ."";// gives the user a message if the connection fails
+}
